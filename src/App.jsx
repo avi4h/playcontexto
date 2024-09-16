5058aa2aacc9e8ef770072f6808174ebef97c5b8
Avi <93165148+avi4h@users.noreply.github.com>
2024-09-16 22:30:24 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import MainSection from './MainSection'
+
+export default function App() {
+  return (
+    <div className='flex justify-center font-nunito bg-con-400 h-screen w-screen'>
+      <MainSection />
+    </div>
+  )
+}
---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        nunito: ["Nunito", "sans-serif"],
+      },
+    },
+    colors: {
+      'con': {
+        200:'#fffefe',
+        400:'#fffbf5',
+        600:'#f4ede2',
+        900:'#423232'
+      }
+    },
+  },
+  plugins: [],
+}
+
